666dc4f358577bc3e7b56e8f74f720d5267851b4
dreamcmi <1912544842@qq.com>
2021-09-23 10:26:36 +0800

    init: esp32s3 begin

---
+++ README.md
@@ -0,0 +1,33 @@
+# LuatOS For ESP32
+
+## 1、简介
+
+尝试给ESP32适配一个[LuatOS](https://gitee.com/openLuat/LuatOS)，只是为了学习。希望有更多的朋友来一起贡献。
+
+目前基于ESP-IDF 4.3 release版本
+
+当前分支ESP32C3，为主力版本。
+
+## 2、参与方式
+
+[UserGuide](./doc/userguide.md)
+
+欢迎提issue和pr帮助项目进步。
+
+## 3、进度
+
+[Support](./doc/SUPPORT.md)
+
+进度更新中，欢迎提需求。
+
+## 4、鸣谢
+
+排名不分前后
+
+- [LuatOS](https://gitee.com/openLuat/LuatOS) ：合宙LuatOS是运行在嵌入式硬件的实时操作系统,用户编写lua代码就可完成各种功能
+
+- [espressif ](https://www.espressif.com/): 乐鑫科技官网，提供高性价比高可玩性的ESP32系列产品
+
+- [Wendal](https://gitee.com/wendal) ：技术大佬，LuatOS领头人
+
+  
---
+++ components/luat/port/luat_uart_esp32.c
@@ -0,0 +1,110 @@
+#include "luat_base.h"
+#include "luat_uart.h"
+#include "luat_log.h"
+#define LUAT_LOG_TAG "luat.uart"
+
+#include "driver/uart.h"
+
+int luat_uart_setup(luat_uart_t *uart)
+{
+    uart_config_t uart_config = {};
+    uart_config.baud_rate = uart->baud_rate;
+    switch (uart->data_bits)
+    {
+    case 8:
+        uart_config.data_bits = UART_DATA_8_BITS;
+        break;
+    case 7:
+        uart_config.data_bits = UART_DATA_7_BITS;
+        break;
+    default:
+        LLOGE("error uart.data_bits");
+        return -1;
+        break;
+    }
+
+    switch (uart->parity)
+    {
+    case LUAT_PARITY_NONE:
+        uart_config.parity = UART_PARITY_DISABLE;
+        break;
+    case LUAT_PARITY_ODD:
+        uart_config.parity = UART_PARITY_ODD;
+        break;
+    case LUAT_PARITY_EVEN:
+        uart_config.parity = UART_PARITY_EVEN;
+        break;
+    default:
+        LLOGE("error uart.parity");
+        return -1;
+        break;
+    }
+
+    uart_config.stop_bits = uart->stop_bits;
+    uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    uart_config.source_clk = UART_SCLK_APB,
+
+    uart_driver_install(uart->id, uart->bufsz, 0, 0, NULL, 0);
+    uart_param_config(uart->id, &uart_config);
+
+    switch (uart->id)
+    {
+    case 1:
+        uart_set_pin(1, 7, 6, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+        break;
+    default:
+        LLOGE("error uart.id");
+        return -1;
+        break;
+    }
+    return 0;
+}
+
+int luat_uart_write(int uartid, void *data, size_t length)
+{
+    if (luat_uart_exist(uartid))
+    {
+        int err = uart_write_bytes(uartid, (const char *)data, length);
+        // printf("uart write :%d\r\n",err);
+        if (err == -1) return -1;
+        else return 0;
+    }
+    else return -1;
+}
+
+int luat_uart_read(int uartid, void *buffer, size_t length)
+{
+    if (luat_uart_exist(uartid))
+    {
+        int err = uart_read_bytes(uartid, buffer, length, 20 / portTICK_RATE_MS);
+        // printf("uart read :%d\r\n",err);
+        if (err == -1) return -1;
+        else return 0;
+    }
+    else return -1;
+}
+
+int luat_uart_close(int uartid)
+{
+    if (luat_uart_exist(uartid))
+    {
+        uart_driver_delete(uartid);
+        return 0;
+    }
+    else return -1;
+}
+
+int luat_uart_exist(int uartid)
+{
+    if (uartid == 1) return 1;
+    else
+    {
+        luat_log_warn("luat.uart", "uart%d not exist", uartid);
+        return 0;
+    }
+}
+
+int luat_setup_cb(int uartid, int received, int sent)
+{
+    return 0;
+}
